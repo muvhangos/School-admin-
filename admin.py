197d4efea614cf5c8143af633661c86cdc93bd70
Samuel Muvhango <93419130+muvhangos@users.noreply.github.com>
2025-07-29 13:03:40 +0200

    Delete admin.py

--- admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from .models import Application
-
-@admin.register(Application)
-class ApplicationAdmin(admin.ModelAdmin):
-    list_display = ('name', 'email', 'grade', 'parent', 'motivation')
